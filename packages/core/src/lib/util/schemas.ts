24410f8c166303f00bc937071cfe13b3f6d4d2fd
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2024-03-01 08:52:23 +0000

    fix decimal schema

--- packages/core/src/lib/util/schemas.ts
+++ packages/core/src/lib/util/schemas.ts
@@ -1,18 +1,21 @@
 import * as ParseResult from '@effect/schema/ParseResult';
 import { Decimal } from 'decimal.js';
-import { AST, Schema } from '@effect/schema';
+import { AST, Arbitrary, Pretty, Schema } from '@effect/schema';
 
 export const walLsnFromString = Schema.transformOrFail(
   Schema.string,
   Schema.bigintFromSelf,
-  (s) => {
+  (s, options, ast) => {
     const match = s.match(/^([A-F0-9]+)\/([A-F0-9]+)$/);
     if (match) {
       const top = BigInt(Number.parseInt(match[1], 16));
       const bottom = BigInt(Number.parseInt(match[2], 16));
       return ParseResult.succeed((top << 32n) + bottom);
     }
-    return ParseResult.fail(ParseResult.unexpected(s));
+
+    return ParseResult.fail(
+      ParseResult.transform(ast, s, 'From', ParseResult.type(ast, s))
+    );
   },
   (i) =>
     ParseResult.succeed(
@@ -25,27 +28,19 @@ export const walLsnFromString = Schema.transformOrFail(
 );
 
 export const DecimalFromSelf: Schema.Schema<Decimal> = Schema.declare(
-  [],
-  Schema.struct({}),
-  () => (u, _, ast) =>
-    Decimal.isDecimal(u)
-      ? ParseResult.succeed(u)
-      : ParseResult.fail(ParseResult.type(ast, u)),
+  Decimal.isDecimal,
   {
-    [AST.IdentifierAnnotationId]: 'Decimal',
-    [AST.MessageAnnotationId]: 'a decimal',
+    identifier: 'DecimalFromSelf',
+    description: 'a decimal',
+    pretty: (): Pretty.Pretty<Decimal> => (decimal) =>
+      `new Decimal(${JSON.stringify(decimal)})`,
   }
 );
 
-export const DecimalFromString = Schema.transformOrFail(
-  Schema.string,
-  DecimalFromSelf,
-  (s) => {
-    try {
-      return ParseResult.succeed(new Decimal(s));
-    } catch (e) {
-      return ParseResult.fail(ParseResult.unexpected(s));
-    }
-  },
-  (d) => ParseResult.succeed(d.toString())
-);
+export const DecimalFromString: Schema.Schema<Decimal, string> =
+  Schema.transform(
+    Schema.string,
+    DecimalFromSelf,
+    (s) => new Decimal(s),
+    (n) => n.toString()
+  ).pipe(Schema.identifier('DecimalFromString'));
